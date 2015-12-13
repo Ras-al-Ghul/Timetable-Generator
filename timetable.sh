c47ca9ffb869b1ce40764763fa372e1817cb0ae7
Ras-al-Ghul <dipankar1995@gmail.com>
2015-12-13 16:12:37 +0530

    Modified timetable.sh

--- timetable.sh
+++ timetable.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 echo "Enter Choice.. 0 for Even Sem, 1 for Odd Sem, Any other to quit :"
 read input_var
-
-if [ "input_var" = "0" ]
+if [ "$input_var" -eq "0" ]
 then
 ./Evensem/Algorithm/evensem.exe
-elif [ "input_var" = "1" ]
+elif [ "$input_var" = "1" ]
 then
 ./Oddsem/Algorithm/oddsem.exe
 else
 exit
 fi
-
